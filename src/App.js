de6e94145d085acd26aeb8bd44470a241e0516d6
Denys Medynskyi <denys.medynskyi@toptal.com>
2019-09-02 12:27:58 +0300

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import Typography from "@material-ui/core/Typography";
 import './App.css';
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
 import useTodoState from "./useTodoState";
+import { Router } from "@reach/router";
+import NavBar from "./NavBar";
 
 function App() {
   const { todos, addTodo, deleteTodo } = useTodoState([]);
 
   return (
     <div className="App">
-      <Typography component="h1" variant="h2">
-        Todos
-      </Typography>
+      <NavBar />
+      <Router>
+        <TodoList path="/" todos={todos} deleteTodo={deleteTodo} />
+        <TodoForm
+          path="/todos/new"
+          saveTodo={todoText => {
+            const trimmedText = todoText.trim();
 
-      <TodoForm
-        saveTodo={todoText => {
-          const trimmedText = todoText.trim();
-
-          if (trimmedText.length > 0) {
-            addTodo(trimmedText);
-          }
-        }}
-      />
-
-      <TodoList todos={todos} deleteTodo={deleteTodo} />
+            if (trimmedText.length > 0) {
+              addTodo(trimmedText);
+            }
+          }}
+        />
+      </Router>
     </div>
   );
 }
